57e72cbd8a6f41186efd1221ebe1ab35494343c8
wicaksono bayu aji <98096095+wicaksuu@users.noreply.github.com>
2025-08-14 09:28:59 +0000

    remove folder

--- src/hv_control.h
+++
@@ -1,7 +0,0 @@
-#ifndef HV_CONTROL_H
-#define HV_CONTROL_H
-
-// Modul ini mengatur jendela pengisian tegangan tinggi sebelum percikan.
-void armHVWindow(double dwellMs);
-
-#endif // HV_CONTROL_H
--- src/ignition_scheduler.h
+++
@@ -1,9 +0,0 @@
-#ifndef IGNITION_SCHEDULER_H
-#define IGNITION_SCHEDULER_H
-
-#include "types.h"
-
-// Modul ini mengatur pemicu percikan berdasarkan sudut advance.
-void scheduleSpark(double advanceDeg);
-
-#endif // IGNITION_SCHEDULER_H
